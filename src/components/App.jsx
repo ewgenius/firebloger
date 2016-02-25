ce553b32a709676950df453caa534a17b2d5c097
ewgenius <ewgeniux@gmail.com>
2016-02-25 13:03:31 +0300

    init component

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import 'normalize.css';
+import 'styles/App.scss';
+
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+      </div>
+    );
+  }
+}
+
+App.defaultProps = {
+};
+
+export default App;
